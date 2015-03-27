0cc3f5b76a2c71fde811a321c0cf50e5d769ff99
gnut3ll4 <thibaut.tauveron@gmail.com>
2015-03-27 11:19:15 -0400

    Incrementation du compteur

---
+++ app/src/main/java/com/gnut3ll4/formationbasique/MainActivity.java
@@ -0,0 +1,59 @@
+package com.gnut3ll4.formationbasique;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    private TextView tvCompteur;
+    private Button btnUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvCompteur = (TextView) findViewById(R.id.tv_compteur);
+        btnUp = (Button) findViewById(R.id.btn_up);
+
+        btnUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count = Integer.valueOf((String) tvCompteur.getText());
+                count++;
+                tvCompteur.setText(""+count);
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
